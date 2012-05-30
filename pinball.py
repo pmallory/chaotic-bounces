b252349fe27989b08da7ad10dea4f2e600d6fee4
Philip Mallory <philip@pmallory.com>
2012-05-30 14:14:46 -0400

    Add two helper methods
    One to help generate triangles of different sizes.
    One to make random unit vectors

--- pinball.py
+++ pinball.py
@@ -1,4 +1,5 @@
 from scipy import *
+from scipy.linalg import norm
 
 def intersect(center, radius, particle_location, particle_velocity):
     intermediate_value = dot(particle_velocity, (center-particle_location))
@@ -17,10 +18,24 @@ def reflect(center, reflection_point, velocity):
     intermediate_value = center - reflection_point
     return v-((dot(2*v, intermediate_value))/(dot(intermediate_value, intermediate_value)))*intermediate_value
 
+def make_circles(side_length):
+    """Given a side length, return the coordinates of the three points of an
+       equilateral triangle centered on the origin."""
+    return [(side_length/2, -side_length*sqrt(3)/6),
+            (-side_length/2, -side_length*sqrt(3)/6),
+            (0, side_length*sqrt(3)/3)]
+
+def generate_unit_vector():
+    """Generate a random (evenly distributed) unit vector. Used to seed a 
+        particle's velocity."""
+    vector = random.random((1,2))
+    return vector/norm(vector)
+
 
 if __name__ == '__main__':
-    center = array([1, 0])
-    radius = 1 
-    particle_location = array([0,0])
-    particle_velocity= array([1,0])
-    print intersect(center, radius, particle_location, particle_velocity)
+    circle_distance = 6
+    circle_radius = 1
+
+    u =  generate_unit_vector()
+    print u
+    print norm(u)
